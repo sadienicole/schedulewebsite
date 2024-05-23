62423ae16900de9fd6227095c27372d7e8a55782
sadienicole <sadienmanicom@gmail.com>
2024-05-22 17:26:37 -0700

    create and maintain variables for selected options, expand jquery handlers

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+let isWorkSelected = false;
+let isSchoolSelected = false;
+let isAppointmentSelected = false;
+let isOtherSelected = false;
+
 $(function(){
 
     $("#workSelectButton").on("click", function(){
         if($('#workSelectCard').hasClass('shadow')) {
+            isWorkSelected = false;
             $('#workSelectCard').removeClass('shadow');
             $('#workSelectCard').removeClass('bg-light');
         }
         else {
+            isWorkSelected = true;
             $('#workSelectCard').addClass('shadow');
             $('#workSelectCard').addClass('bg-light');
         }
@@ -13,10 +20,12 @@ $(function(){
 
       $("#schoolSelectButton").on("click", function(){
         if($('#schoolSelectCard').hasClass('shadow')) {
+            isSchoolSelected = false;
             $('#schoolSelectCard').removeClass('shadow');
             $('#schoolSelectCard').removeClass('bg-light');
         }
         else {
+            isSchoolSelected = true;
             $('#schoolSelectCard').addClass('shadow');
             $('#schoolSelectCard').addClass('bg-light');
         }
@@ -24,10 +33,12 @@ $(function(){
 
       $("#appointmentSelectButton").on("click", function(){
         if($('#appointmentSelectCard').hasClass('shadow')) {
+            isAppointmentSelected = false;
             $('#appointmentSelectCard').removeClass('shadow');
             $('#appointmentSelectCard').removeClass('bg-light');
         }
         else {
+            isAppointmentSelected = true;
             $('#appointmentSelectCard').addClass('shadow');
             $('#appointmentSelectCard').addClass('bg-light');
         }
@@ -35,13 +46,16 @@ $(function(){
 
       $("#otherSelectButton").on("click", function(){
         if($('#otherSelectCard').hasClass('shadow')) {
+            isOtherSelected = false;
             $('#otherSelectCard').removeClass('shadow');
             $('#otherSelectCard').removeClass('bg-light');
         }
         else {
+            isOtherSelected = true;
             $('#otherSelectCard').addClass('shadow');
             $('#otherSelectCard').addClass('bg-light');
         }
       });
 
-  });
+});
+
